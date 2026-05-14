4a0e513bdb6bec6bd29c97869cb17242a088e19d
aiiiden <aiiiden.dev@gmail.com>
2026-05-14 13:10:00 +0900

    chore(frontend): simplify state

--- apps/catarchy-frontend/src/features/play/screens/play-screen.tsx
+++ apps/catarchy-frontend/src/features/play/screens/play-screen.tsx
@@ -2,7 +2,6 @@ import { PowerOffButton } from "@/features/auth";
 import { LogoText, Scaffold } from "@/features/common";
 
 import { catInfoOptions } from "@/features/cat";
-import { SummonForm } from "@/features/cat/components/summon-form";
 import { useQuery } from "@tanstack/react-query";
 import { CareButton } from "../components/care-button";
 import { CatLoading } from "../components/cat-loading";
@@ -10,10 +9,9 @@ import { Interface } from "../components/interface";
 import styles from "./play-screen.module.css";
 
 export function PlayScreen() {
-  const { status: catInfoStatus, error: catInfoError } =
-    useQuery(catInfoOptions());
+  const { status } = useQuery(catInfoOptions());
 
-  const hasNoCat = catInfoStatus === "error" && catInfoError?.status === 404;
+  const isLoading = status === "pending";
 
   return (
     <Scaffold className="bg-pattern-cat">
@@ -25,13 +23,12 @@ export function PlayScreen() {
       <Scaffold.Body className={styles.body}>
         <div className={styles.content}>
           <div className={styles.centerFlex}>
-            {catInfoStatus === "pending" && <CatLoading />}
-            {catInfoStatus === "success" && <Interface />}
-            {hasNoCat && <SummonForm />}
+            {isLoading && <CatLoading />}
+            {!isLoading && <Interface />}
           </div>
         </div>
       </Scaffold.Body>
-      {catInfoStatus === "success" && (
+      {!isLoading && (
         <Scaffold.Bottom sticky>
           <CareButton />
         </Scaffold.Bottom>
